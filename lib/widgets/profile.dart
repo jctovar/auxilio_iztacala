d5299b8de6b37e02d22c5ae5747c78dc5b5e6585
jctovar <jctovar@gmail.com>
2023-06-07 22:07:28 -0600

    20230607

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -1,5 +1,6 @@
 import 'package:alerta/globals/environment.dart';
 import 'package:alerta/services/helper_functions.dart';
+import 'package:alerta/utils/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
@@ -51,15 +52,15 @@ class _ProfileState extends State<Profile> {
           children: <Widget>[
             const SizedBox(height: 20),
             FormBuilderTextField(
-                name: 'username',
-                key: Key(_username.toString()),
-                initialValue: _username,
-                decoration: Environment.inputDecoration(
-                    'Número de cuenta', 'Ingresa tu número de cuenta'),
-                validator: FormBuilderValidators.compose([
-                  FormBuilderValidators.required(),
-                ]),
-                onChanged: _saveData()),
+              name: 'username',
+              key: Key(_username.toString()),
+              initialValue: _username,
+              decoration: Environment.inputDecoration(
+                  'Número de cuenta', 'Ingresa tu número de cuenta'),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+              ]),
+            ),
             const SizedBox(height: 10),
             FormBuilderTextField(
               name: 'email',
@@ -70,6 +71,26 @@ class _ProfileState extends State<Profile> {
                 FormBuilderValidators.required(),
                 FormBuilderValidators.email(),
               ]),
+            ),
+            const SizedBox(height: 40),
+            SizedBox(
+              height: 50,
+              width: 250,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.indigo, // Background color
+                ),
+                onPressed: () {
+                  if (_formKey.currentState?.saveAndValidate() ?? false) {
+                    Logs.p.i(_formKey.currentState?.value);
+                    //_authLogin(_formKey.currentState?.value);
+                  }
+                },
+                child: const Text(
+                  'Guardar datos',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
             )
           ],
         ));
